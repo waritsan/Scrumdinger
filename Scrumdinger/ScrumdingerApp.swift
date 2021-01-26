8dff863ecd12bd1e30fdab4d886795c2c32d82f2
Warit Santaputra <waritsan@gmail.com>
2021-01-26 09:11:42 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Warit Santaputra on 26/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
