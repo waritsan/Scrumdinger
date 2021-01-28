449590a82479b1c4d5cbbd4ff922148f49d23c59
Warit Santaputra <waritsan@gmail.com>
2021-01-28 20:51:04 +0700

    Add MeetingHeaderView

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -8,42 +8,31 @@
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+            VStack {
+
+                Circle()
+                    .strokeBorder(lineWidth: 24, antialiased: true)
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}, label: {
+                        Image(systemName: "forward.fill")
+                    })
+                    .accessibilityLabel(Text("Next speaker"))
                 }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("600", systemImage: "hourglass.tophalf.fill")
-                }
-            }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel(Text("Time Remaining"))
-            .accessibilityValue(Text("10 minutes"))
-            Circle()
-                .strokeBorder(lineWidth: 24, antialiased: true)
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}, label: {
-                    Image(systemName: "forward.fill")
-                })
-                .accessibilityLabel(Text("Next speaker"))
             }
         }
         .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        MeetingView(scrum: .constant(DailyScrum.data[0]))
     }
 }
